0d30c1e49346ea8b5c08270edfd072c233fb389e
imHireki <imhireki@gmail.com>
2022-01-02 10:05:16 -0300

    refactor: update the main class

--- wall.py
+++ wall.py
@@ -4,16 +4,20 @@ from PIL import Image
 
 
 class WallpaperHandler:
-    wallpaper_path = '~/.wallpaper.jpg'
-    wallpaper = ...
+    wallpaper_fp = str
+    image_obj = object
+
+    def set_image_obj(self) -> object:
+        with Image.open(self.wallpaper_fp) as pil_image:
+            self.image_obj = pil_image
+            return
 
     @classmethod
-    def set_wallpaper(cls) -> object:
-        with Image.open(cls.wallpaper_path) as wallpaper:
-            cls.wallpaper = wallpaper
-            return
+    def get_color_palette(cls):
+        ...
 
 
 if __name__ == '__main__':
-    wall = WallpaperHandler()
-    wall.set_wallpaper()
+    w = WallpaperHandler()
+    w.wallpaper_fp = ''
+    w.set_image_obj()
