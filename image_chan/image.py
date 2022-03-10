3335db96d3e6cdadc58e8a87a713e92964316673
imHireki <imhireki@gmail.com>
2022-03-10 18:04:49 -0300

    [fix]: Remove useless GetColors from image module

--- image_chan/image.py
+++ image_chan/image.py
@@ -75,26 +75,6 @@ class Icon(_Image):
 class Wallpaper(_Image): pass
 
 
-
-
-class GetColors:
-    def __init__(self, image, clusters=5):
-        self.ic = ImageColor(img=image, clusters=clusters)
-
-    def palette(self):
-        """ Return the color palette """
-        hex_colors = self.ic.hex_color()
-        incidences = self.ic.incidences()
-        colors_incidences = self.ic.colors_incidences(hex_colors, incidences)
-        return self.ic.sorted_colors(colors_incidences)
-
-    def dominant_color(self):
-        """ Return the most common color """
-        incidences = self.ic.incidences()
-        dominant = self.ic.dominant_color(incidences)
-        return self.ic.hex_color([dominant])[0]
-
-
 class Bulk:
     def __init__(self, objs):
         self.objs = objs
