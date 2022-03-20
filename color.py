91691e023f56028c0fa18ad7367952ac3b86ca4e
imHireki <imhireki@gmail.com>
2022-03-20 18:27:11 -0300

    [refactor]: improve performance and remove bugs

--- color.py
+++ color.py
@@ -5,81 +5,73 @@ from scipy import cluster
 
 
 class ImageColor:
-    def __init__(self, **kwargs):
-        self.img = kwargs.get('img', None)
-        self.img_asarray = self.image_as_array()
-        self.color_cluster = self.color_cluster(kwargs.get('clusters'))
+    def __init__(self, image, clusters):
+        self.image_array = self._image_array(image)
+        self.color_cluster = self._color_cluster(clusters)
 
-    def image_as_array(self):
-        """ Return the image as array """
-        ar = asarray(self.img)
-        shape = ar.shape
-        return ar.reshape(product(shape[:2]),
-                          shape[2]).astype(float)
+    def _image_array(self, image):
+        ar = asarray(image)
+        return ar.reshape(product(ar.shape[:2]), ar.shape[2]).astype(float)
 
-    def color_cluster(self, clusters:int):
-        """ Return the cluster of colors """
-        return cluster.vq.kmeans(self.img_asarray, clusters)[0]
+    def _color_cluster(self, clusters):
+        return cluster.vq.kmeans2(self.image_array, clusters)[0]
+
+    @property
+    def incidences(self):
+        """ Return the color's incidences """
+        vecs = cluster.vq.vq(self.image_array, self.color_cluster)[0]
+        return histogram(vecs, len(self.color_cluster))[0]
+
+    def hexify_rgb_array(self, rgb):
+        # remove bytearray problems
+        rgb_list = [int(value) for value in rgb if 256 > int(value) > 0]
+        if len(rgb_list) != 3:
+            return None
+        return hexlify(bytearray(rgb_list)).decode('ascii')
+
+    def hex_color(self, cluster=None):
+        # Remove None
+        return [
+            f'#{hex_rgb}' for rgb in cluster or self.color_cluster
+            for hex_rgb in [self.hexify_rgb_array(rgb)] if hex_rgb
+        ]
 
     def dominant_color(self, color_incidences):
         """ Return the most common color """
-        index_max = argmax(color_incidences)
-        peak = self.color_cluster[index_max]
-        return peak
+        return self.color_cluster[argmax(color_incidences)]
 
-    def incidences(self):
-        """ Return the color's incidences """
-        vecs = cluster.vq.vq(self.img_asarray, self.color_cluster)[0]
-        return histogram(vecs, len(self.color_cluster))[0]
-
-    def hex_color(self, cluster=None) -> list:
-        """ Return the color cluster converted to hex """
-        if not cluster: cluster = self.color_cluster
-        return [
-            '#' +
-            str(hexlify(bytearray((int(_) for _ in color))
-                        ).decode('ascii')
-                )
-            for color in cluster
-        ]
+    def colors_incidences(self, colors, incidences):
+        """ Return list with colors and its incidences
+        Put it all back together as [ ('color', 'incidences'), ]
+        """
+        return map(self.attach_colors_incidences, colors, incidences)
 
     @staticmethod
-    def colors_incidences(colors, incidences):
-        """ Return list with colors and its incidences """
-
-        # Put it all back together as [ ('color', 'incidences'), ]
-        return [ (x, y)
-                 for a, b in enumerate(colors)
-                 for x, y in [
-                     (colors[a], incidences[a])
-                     ]
-                 ]
+    def attach_colors_incidences(colors, incidences):
+         return (colors, incidences)
 
     @staticmethod
     def sorted_colors(color_incidences):
         """ Return sorted colors by its incidences """
-
-        # sort color_incidences by its numbers of incidences
-        sorted_color_incidences = sorted(color_incidences,
-                                         key=lambda x: x[1],
-                                         reverse=True)
-        # Return just the colors
-        return [_[0] for _ in sorted_color_incidences]
+        return [
+            _[0]
+            for _ in sorted(color_incidences, key=lambda x: x[1], reverse=True)
+        ]
 
 
-class GetColors:
-    def __init__(self, image, clusters=5):
-        self.ic = ImageColor(img=image, clusters=clusters)
+class Colors:
+    def __init__(self, image, clusters=10):
+        self.ic = ImageColor(image=image, clusters=clusters)
 
     def palette(self):
         """ Return the color palette """
-        hex_colors = self.ic.hex_color()
-        incidences = self.ic.incidences()
-        colors_incidences = self.ic.colors_incidences(hex_colors, incidences)
-        return self.ic.sorted_colors(colors_incidences)
+        return self.ic.sorted_colors(
+            self.ic.colors_incidences(self.ic.hex_color(), self.ic.incidences)
+        )
 
     def dominant_color(self):
         """ Return the most common color """
-        incidences = self.ic.incidences()
-        dominant = self.ic.dominant_color(incidences)
-        return self.ic.hex_color([dominant])[0]
+        return self.ic.hex_color(
+            [self.ic.dominant_color(self.ic.incidences)]
+        )[0]
+
