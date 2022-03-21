0ae1eee903eb4412dc3bce044cb38f66652db3e5
imHireki <imhireki@gmail.com>
2022-03-21 12:29:54 -0300

    [doc]: add docs to ColorCluster

--- color.py
+++ color.py
@@ -1,81 +1,126 @@
 #!/usr/bin/env python3
-from numpy import asarray, product, histogram, argmax
+from numpy import asarray, product, histogram, argmax, ndarray
 from binascii import hexlify
 from scipy import cluster
-import PIL.Image
+from typing import List, Dict, Union, Mapping
 
 
-class ImageColor:
-    def __init__(self, image, clusters):
-        self.image_array = self.image_array(image)
-        self.color_cluster = self.color_cluster(clusters)
-
-    @property
-    def incidences(self):
-        """ Return the color's incidences """
-        vecs = cluster.vq.vq(self.image_array, self.color_cluster)[0]
-        return histogram(vecs, len(self.color_cluster))[0]
-
-    def image_array(self, image):
-        ar = asarray(image)
-        return ar.reshape(product(ar.shape[:2]), ar.shape[2]).astype(float)
-
-    def color_cluster(self, clusters):
-        return cluster.vq.kmeans2(self.image_array, clusters)[0]
-
-    def hexify_rgb_array(self, rgb):
-        rgb_list = [int(value) for value in rgb if 256 > int(value) > 0]
-        if len(rgb_list) != 3:
-            return None
-        return hexlify(bytearray(rgb_list)).decode('ascii')
-
-    def hex_color(self, cluster=None):
-        return [
-            f'#{hex_rgb}' for rgb in cluster or self.color_cluster
-            for hex_rgb in [self.hexify_rgb_array(rgb)] if hex_rgb
-        ]
-
-    def dominant_color(self, color_incidences):
-        """ Return the most common color """
-        return self.color_cluster[argmax(color_incidences)]
-
-    @staticmethod
-    def colors_incidences(colors, incidences):
-        return map(lambda c, i: (c, i), colors, incidences)
-
-    @staticmethod
-    def sorted_colors(color_incidences):
-        """ Return sorted colors by its incidences """
-        return [
-            _[0]
-            for _ in sorted(color_incidences, key=lambda x: x[1], reverse=True)
-        ]
-
-
-class Colors:
-    """Handle an ImageColor object to get image's colors.
+class ColorCluster:
+    """Manage image's color cluster.
 
     Args:
         image (PIL.Image.Image): The image to extract the colors.
         clusters (int): The amount of color cluster to split the image.
 
     Attributes:
-        ic (ImageColor): The object to get the colors.
+        image_array (numpy.ndarray): The image as array.
+        color_cluster (numpy.ndarray): A cluster with colors from the image.
     """
 
-    def __init__(self, image, clusters=10):
-        self.ic = ImageColor(image=image, clusters=clusters)
+    def __init__(self, image, clusters):
+        self.image_array = self.image_array(image)
+        self.color_cluster = self.color_cluster(clusters)
 
     @property
-    def palette(self):
+    def incidences(self) -> ndarray:
+        """Return the incidences of the cluster's colors."""
+
+        vecs = cluster.vq.vq(self.image_array, self.color_cluster)[0]
+        return histogram(vecs, len(self.color_cluster))[0]
+
+    def image_array(self, image) -> ndarray:
+        """Return the image as array."""
+
+        ar = asarray(image)
+        return ar.reshape(product(ar.shape[:2]), ar.shape[2]).astype(float)
+
+    def color_cluster(self, clusters:int) -> ndarray:
+        """Return a cluster with colors from the image."""
+
+        return cluster.vq.kmeans2(self.image_array, clusters)[0]
+
+    def hexify_rgb_array(self, rgb:ndarray) -> str:
+        """Return the hex of the given rgb."""
+
+        rgb_list = [int(value) for value in rgb if 256 > int(value) > 0]
+        if len(rgb_list) != 3:
+            return None
+        return hexlify(bytearray(rgb_list)).decode('ascii')
+
+    def hex_color(self, color_cluster=None) -> List[str]:
+        """Return a list with the hex from the color cluster.
+
+        If color_cluster wasn't specified, return the hex
+        from `self.color_cluster`.
+
+        Args:
+            color_cluster (ndarray): The color cluster to be transformed as
+                hex colors.
+        """
+
+        return [
+            f'#{hex_rgb}' for rgb in color_cluster or self.color_cluster
+            for hex_rgb in [self.hexify_rgb_array(rgb)] if hex_rgb
+        ]
+
+    def dominant_color(self, incidences:ndarray) -> ndarray:
+        """Return the most common color across the whole image."""
+
+        return self.color_cluster[argmax(incidences)]
+
+    @staticmethod
+    def colors_incidences(colors, incidences) -> Mapping[str, int]:
+        """Return a mapping with color and its incidences.
+
+        Args:
+            colors (List[str]): A list with hex colors.
+            incidences (ndarray): The incidences of each hex color.
+        """
+
+        return map(lambda c, i: (c, i), colors, incidences)
+
+    @staticmethod
+    def sorted_colors(color_incidences:map) -> List[str]:
+        """Return the colors sorted by its incidences.
+
+        Args:
+            color_incidences (Mapping[str, int]): The colors and
+                its incidences.
+        """
+
+        return [
+            _[0] for _ in sorted(
+                color_incidences, key=lambda x: x[1], reverse=True
+            )
+        ]
+
+
+class Colors:
+    """Handle an ColorCluster object to get image's colors.
+
+    Args:
+        image (PIL.Image.Image): The image to extract the colors.
+        clusters (int): The amount of color cluster to split the image.
+
+    Attributes:
+        ic (ColorCluster): The object to get the colors.
+    """
+
+    def __init__(self, image, clusters=5):
+        self.ic = ColorCluster(image=image, clusters=clusters)
+
+    @property
+    def palette(self) -> List[str]:
         """Return the color palette based on the number of clusters."""
+
         return self.ic.sorted_colors(
             self.ic.colors_incidences(self.ic.hex_color(), self.ic.incidences)
         )
 
     @property
-    def dominant_color(self):
+    def dominant_color(self) -> str:
         """Return the most common color."""
+
         return self.ic.hex_color(
             [self.ic.dominant_color(self.ic.incidences)]
         )[0]
