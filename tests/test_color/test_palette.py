abd87565cfa5fe585a7fdd4c8edcf3e11739a6ca
imHireki <imhireki@gmail.com>
2022-07-13 15:14:27 -0300

    [test]: add test_range_color_palette

--- tests/test_color/test_palette.py
+++ tests/test_color/test_palette.py
@@ -11,3 +11,16 @@ def test_dominant_color(mocker):
 
     assert dominant_color.get_palette_data() == sorted_colors[0]
     assert dominant_color.get_palette_data_as_hex() == dominant_color_as_hex
+
+
+def test_range_color_palette(mocker):
+    sorted_colors = [(255, 255, 255), (255, 0, 0), (0, 255, 0), (0, 0, 255), (0, 0, 0)]
+    color_cluster = mocker.Mock(sort_colors_by_incidences=lambda: sorted_colors)
+
+    range_color_palette = palette.RangeColorPalette(color_cluster)
+
+    sorted_colors_as_hex = [range_color_palette._hexlify_rgb(color)
+                            for color in sorted_colors]
+
+    assert range_color_palette.get_palette_data(5, 3, 2) == sorted_colors[3:5:2]
+    assert range_color_palette.get_palette_data_as_hex(2) == sorted_colors_as_hex[:2]
