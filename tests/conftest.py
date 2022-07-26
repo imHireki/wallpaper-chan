9e4efb16c4a042495214f9fcd625c6bf4f6e22d9
imHireki <imhireki@gmail.com>
2022-07-26 07:54:12 -0300

    [feat]: add convert_mode_options to the editor_options fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,5 +7,6 @@ def editor_options():
         "resize_options": {
             "size": (512, 512), "resample": 1, "reducing_gap": 2
         },
+        "convert_mode_options": {"mode": "rgb"},
         "save_options": {"quality": 75, "format": "webp"}
     }
