22370bbd9f75b9b48e92086397defb8c2a07d7d1
imHireki <imhireki@gmail.com>
2022-07-25 23:07:20 -0300

    [refactor]: replace editor_options_mock fixture for the editor_options fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,7 +2,10 @@ import pytest
 
 
 @pytest.fixture
-def editor_options_mock(mocker):
-    resize_options = {"size": (512, 512), "resample": 2, "reducing_gap": 2}
-    save_options = {"quality": 75, "format": "WEBP"}
-    return mocker.Mock(resize_options=resize_options, save_options=save_options)
+def editor_options():
+    return {
+        "resize_options": {
+            "size": (512, 512), "resample": 1, "reducing_gap": 2
+        },
+        "save_options": {"quality": 75, "format": "webp"}
+    }
