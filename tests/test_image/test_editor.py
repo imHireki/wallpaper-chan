853e6ce993d91bb2eedc1202ded5eb20f3822a7a
imHireki <imhireki@gmail.com>
2022-08-01 11:05:46 -0300

    [refactor]: remove useless mocks from test_animated_image_editor

--- tests/test_image/test_editor.py
+++ tests/test_image/test_editor.py
@@ -22,27 +22,20 @@ def test_static_image_editor(mocker, editor_options):
     assert image_save_mock.call_args.kwargs == editor_options["save_options"]
 
 def test_animated_image_editor(mocker, editor_options):
-    image_resize_mock = mocker.Mock()
-    frame_convert_mock = mocker.Mock()
-    image_sequence_iterator_mock = mocker.patch(
-        'PIL.ImageSequence.Iterator',
-        lambda _: (frame for frame in [mocker.Mock(resize=image_resize_mock,
-                                                   convert=frame_convert_mock)])
-    )
-    animated_image_editor = editor.AnimatedImageEditor(mocker.Mock())
+    image_mock = mocker.Mock()
+    mocker.patch('PIL.ImageSequence.Iterator', lambda _: (frame for frame in [image_mock]))
+    animated_image_editor = editor.AnimatedImageEditor(image_mock)
 
     animated_image_editor.resize(**editor_options["resize_options"])
-
     animated_image_editor.convert_mode(**editor_options["convert_mode_options"])
-
-    image_save_mock = mocker.patch.object(animated_image_editor._image, 'save')
     animated_image_editor.save(**editor_options["save_options"])
 
     os.remove(animated_image_editor.result.name)
 
-    assert image_resize_mock.call_args.kwargs == editor_options["resize_options"]
-    assert frame_convert_mock.call_args.kwargs == editor_options["convert_mode_options"]
-    assert set(editor_options["save_options"]).issubset(image_save_mock.call_args.kwargs)
+    assert image_mock.resize.call_args.kwargs == editor_options["resize_options"]
+    assert image_mock.convert.call_args.kwargs == editor_options["convert_mode_options"]
+    assert set(editor_options["save_options"]).issubset(
+        animated_image_editor._image.save.call_args.kwargs)
 
 def test_bulk_resize_save_editor(mocker, editor_options):
     image_editor_mock = mocker.Mock()
