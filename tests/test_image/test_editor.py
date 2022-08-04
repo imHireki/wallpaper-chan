c0cf65d30e32c3beae8f3ed88033564b4e822bd8
imHireki <imhireki@gmail.com>
2022-08-04 14:45:38 -0300

    [refactor]: adjust the test_animated_image_editor to fit the _frames modifications

--- tests/test_image/test_editor.py
+++ tests/test_image/test_editor.py
@@ -19,11 +19,17 @@ def test_static_image_editor(mocker, editor_options):
     assert image_to_save_mock.save.call_args.kwargs == editor_options["save_options"]
 
 def test_animated_image_editor(mocker, editor_options):
-    image_mock = mocker.Mock()
-    mocker.patch('PIL.ImageSequence.Iterator', lambda _: (frame for frame in [image_mock]))
+    image_mock = mocker.Mock(mode='P')
+    image_mock.convert = mocker.Mock(return_value=image_mock)
+    mocker.patch('image.editor.AnimatedImageEditor._get_frames',
+                 lambda _: (frame for frame in [image_mock]))
+    mocker.patch('image.editor.AnimatedImageEditor._get_actual_mode', lambda _: 'RGB')
+
     animated_image_editor = editor.AnimatedImageEditor(image_mock)
 
     animated_image_editor.resize(**editor_options["resize_options"])
+    animated_image_editor.save(**editor_options["save_options"])
+
     animated_image_editor.convert_mode(**editor_options["convert_mode_options"])
     animated_image_editor.save(**editor_options["save_options"])
 
@@ -32,7 +38,9 @@ def test_animated_image_editor(mocker, editor_options):
     assert image_mock.resize.call_args.kwargs == editor_options["resize_options"]
     assert image_mock.convert.call_args.kwargs == editor_options["convert_mode_options"]
     assert set(editor_options["save_options"]).issubset(
-        animated_image_editor._image.save.call_args.kwargs)
+        animated_image_editor._original_image.save.call_args.kwargs
+    )
+
 
 def test_bulk_resize_save_editor(mocker, editor_options):
     image_editor_mock = mocker.Mock()
