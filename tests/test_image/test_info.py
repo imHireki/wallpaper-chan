06c573f23bf5123859a72923d919a9ed674a09d7
imHireki <imhireki@gmail.com>
2022-08-09 16:00:25 -0300

    [test]: add TestStaticWebpRgbInfo

--- tests/test_image/test_info.py
+++ tests/test_image/test_info.py
@@ -23,3 +23,20 @@ class TestStaticJpegRgbInfo:
 
     def test_standardize(self, mocker):
         assert not info.StaticJpegRgbInfo(mocker.Mock()).standardize()
+
+
+class TestStaticWebpRgbInfo:
+    def test_name(self, mocker):
+        assert info.StaticWebpRgbInfo(mocker.Mock()).name == 'WEBP_RGB'
+
+    def test_is_standardized(self, mocker):
+        assert info.StaticWebpRgbInfo(mocker.Mock()).is_standardized() is False
+
+    def test_standardize(self, mocker):
+        mocker.patch('image.editor.StaticImageEditor')
+
+        image_info = info.StaticWebpRgbInfo(mocker.Mock())
+        standardized_image = image_info.standardize()
+
+        assert standardized_image is image_info._image_editor.result
+        assert image_info._image_editor.save.call_args.kwargs == info.save_options['JPEG']
