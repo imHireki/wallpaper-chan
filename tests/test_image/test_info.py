639e93affb1ad8bdd821e9259299f1ee439e0a96
imHireki <imhireki@gmail.com>
2022-08-09 15:44:26 -0300

    [test]: test the interface feature on its own test

--- tests/test_image/test_info.py
+++ tests/test_image/test_info.py
@@ -1,16 +1,7 @@
 from image import info
 
 
-class TestStaticJpegRgbInfo:
-    def test_name(self, mocker):
-        assert info.StaticJpegRgbInfo(mocker.Mock()).name == 'JPEG_RGB'
-
-    def test_is_standardized(self, mocker):
-        assert info.StaticJpegRgbInfo(mocker.Mock()).is_standardized() is True
-
-    def test_standardize(self, mocker):
-        assert not info.StaticJpegRgbInfo(mocker.Mock()).standardize()
-
+class TestIStaticImageInfo:
     def test_get_image_editor(self, mocker):
         image_info = info.StaticJpegRgbInfo(mocker.Mock())
         image_editor_mock = mocker.patch('image.editor.StaticImageEditor')
@@ -22,3 +13,13 @@ class TestStaticJpegRgbInfo:
         image_info = info.StaticJpegRgbInfo(mocker.Mock())
         assert image_info.get_image_for_color_clustering() is image_info._image
 
+
+class TestStaticJpegRgbInfo:
+    def test_name(self, mocker):
+        assert info.StaticJpegRgbInfo(mocker.Mock()).name == 'JPEG_RGB'
+
+    def test_is_standardized(self, mocker):
+        assert info.StaticJpegRgbInfo(mocker.Mock()).is_standardized() is True
+
+    def test_standardize(self, mocker):
+        assert not info.StaticJpegRgbInfo(mocker.Mock()).standardize()
