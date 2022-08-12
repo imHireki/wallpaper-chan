c27712294fc82dfbe815eb2d0ba66ab427dc09e6
imHireki <imhireki@gmail.com>
2022-08-12 08:40:35 -0300

    [refactor]: move the TestIStaticImageInfo tests to the TestJpegRgbInfo

--- tests/test_image/test_info.py
+++ tests/test_image/test_info.py
@@ -3,7 +3,16 @@ import pytest
 from image import info
 
 
-class TestIStaticImageInfo:
+class TestStaticJpegRgbInfo:
+    def test_name(self):
+        assert info.StaticJpegRgbInfo.name == 'JPEG_RGB'
+
+    def test_is_standardized(self, mocker):
+        assert info.StaticJpegRgbInfo(mocker.Mock()).is_standardized() is True
+
+    def test_standardize(self, mocker):
+        assert not info.StaticJpegRgbInfo(mocker.Mock()).standardize()
+
     def test_get_image_editor(self, mocker):
         image_info = info.StaticJpegRgbInfo(mocker.Mock())
         image_editor_mock = mocker.patch('image.editor.StaticImageEditor')
@@ -14,17 +23,6 @@ class TestIStaticImageInfo:
     def test_get_image_for_color_clustering(self, mocker):
         image_info = info.StaticJpegRgbInfo(mocker.Mock())
         assert image_info.get_image_for_color_clustering() is image_info._image
-
-
-class TestStaticJpegRgbInfo:
-    def test_name(self):
-        assert info.StaticJpegRgbInfo.name == 'JPEG_RGB'
-
-    def test_is_standardized(self, mocker):
-        assert info.StaticJpegRgbInfo(mocker.Mock()).is_standardized() is True
-
-    def test_standardize(self, mocker):
-        assert not info.StaticJpegRgbInfo(mocker.Mock()).standardize()
 
 
 class TestStaticWebpRgbInfo:
