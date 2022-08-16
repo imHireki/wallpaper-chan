203bd394783284f0a758f7b53b94a459794a4beb
imHireki <imhireki@gmail.com>
2022-08-16 08:20:03 -0300

    [test]: add test_animated_image_support

--- tests/test_image/test_support.py
+++ tests/test_image/test_support.py
@@ -10,3 +10,11 @@ def test_static_image_support(mocker):
 
     assert image_info is supported_images_mock['STATIC']['JPEG_RGB']
 
+def test_animated_image_support(mocker):
+    supported_images_mock = {"ANIMATED": {"GIF_P": mocker.Mock()}}
+    image_mock = mocker.Mock(format='GIF', mode='P')
+
+    image_support = support.AnimatedImageSupport(image_mock, supported_images_mock)
+    image_info = image_support.get_image_info()
+
+    assert image_info is supported_images_mock['ANIMATED']['GIF_P']
