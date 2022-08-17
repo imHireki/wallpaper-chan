bb2feb32945d54f70f5f957ea4ef109063d8a88a
imHireki <imhireki@gmail.com>
2022-08-17 06:47:03 -0300

    [test]: add TestImageSupportProxy

--- tests/test_image/test_support.py
+++ tests/test_image/test_support.py
@@ -1,3 +1,5 @@
+import pytest
+
 from image import support
 
 
@@ -18,3 +20,42 @@ def test_animated_image_support(mocker):
     image_info = image_support.get_image_info()
 
     assert image_info is supported_images_mock['ANIMATED']['GIF_P']
+
+
+class TestImageSupportProxy:
+    def test_get_image_info(self, mocker):
+        proxy = support.ImageSupportProxy(mocker.Mock(), {})
+        image_support_mock = mocker.patch.object(proxy, 'get_image_support')
+        proxy._image_support = mocker.Mock()
+
+        image_info = proxy.get_image_info()
+
+        assert proxy._image_support.get_image_info.called
+        assert  proxy._image_info is image_info
+
+    @pytest.mark.parametrize('_image_info', [1, 0])
+    def test_is_supported(self, mocker, _image_info):
+        proxy = support.ImageSupportProxy(mocker.Mock(), {})
+
+        image_info_mock = mocker.patch.object(proxy, 'get_image_info')
+        proxy._image_info = _image_info
+
+        is_supported = proxy.is_supported()
+
+        assert is_supported is (_image_info is not None)
+
+    @pytest.mark.parametrize('is_animated,format,support_patch', [
+        [True, 'GIF', 'image.support.AnimatedImageSupport'],
+        [False, 'JPEG', 'image.support.StaticImageSupport'],
+        [False, 'GIF', 'image.support.AnimatedImageSupport']
+    ])
+    def test_get_image_support(self, mocker, is_animated, format, support_patch):
+        image_support_mock = mocker.patch(support_patch)
+
+        proxy = support.ImageSupportProxy(
+            mocker.Mock(is_animated=is_animated, format=format), {}
+        )
+        image_support = proxy.get_image_support()
+
+        assert image_support_mock.called
+        assert proxy._image_support is image_support
