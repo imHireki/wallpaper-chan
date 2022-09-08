612fffb513ea76724ff8a8e7d035bb3aa74ef3bf
imHireki <imhireki@gmail.com>
2022-09-08 16:57:01 -0300

    [refactor]: add get_colors to better handle the private methods

--- color/cluster.py
+++ color/cluster.py
@@ -3,33 +3,41 @@ from typing import Sequence, Mapping
 import PIL.Image
 
 
+RGBSequences = tuple[Sequence]
+RGB = tuple[int]
+
+
 class ColorCluster:
-    _image: PIL.Image.Image
-    _rgb_sequences: tuple[Sequence]
-    _colors: Mapping[tuple[Sequence], tuple[int]]
-
     def __init__(self, image: PIL.Image.Image) -> None:
-        self._image = image
-        self._rgb_sequences = self._get_rgb_sequences()
-        self._colors = self._extract_colors()
+        self._image: PIL.Image.Image = image
 
-    def _get_rgb_sequences(self) -> tuple[Sequence]:
+    def get_colors(self) -> list[RGB]:
+        rgb_sequences = self._get_rgb_sequences()
+        colors = self._extract_colors(rgb_sequences)
+        colors_with_incidences = self._calculate_color_incidences(colors)
+        return self._sort_colors_by_incidences(colors_with_incidences)
+
+    def _get_rgb_sequences(self) -> RGBSequences:
         return [self._image.getdata(band) for band in range(3)]
 
-    def _extract_colors(self) -> Mapping[tuple[Sequence], tuple[int]]:
-        return map(lambda *rgb: tuple(rgb), *self._rgb_sequences)
+    def _extract_colors(self, rgb_sequences: RGBSequences
+                        ) -> Mapping[RGBSequences, RGB]:
+        return map(lambda *rgb: tuple(rgb), *rgb_sequences)
 
-    def _calculate_color_incidences(self) -> dict[tuple[int], int]:
-        color_with_incidences: dict[tuple[int], int] = {}
+    def _calculate_color_incidences(
+            self, colors: Mapping[RGBSequences, RGB]) -> dict[RGB, int]:
+        color_with_incidences: dict[RGB, int] = {}
 
-        for color in self._colors:
+        for color in colors:
             if color not in color_with_incidences:
                 color_with_incidences[color] = 1
             else:
                 color_with_incidences[color] += 1
         return color_with_incidences
 
-    def sort_colors_by_incidences(self) -> list[tuple[int]]:
-        return sorted(self._calculate_color_incidences(),
-                      key=lambda color_with_incidences: color_with_incidences[1],
-                      reverse=True)
+    def _sort_colors_by_incidences(
+            self, colors_with_incidences: dict[RGB, int]) -> list[RGB]:
+        return sorted(
+            colors_with_incidences,
+            key=lambda color_with_incidences: color_with_incidences[1],
+            reverse=True)
