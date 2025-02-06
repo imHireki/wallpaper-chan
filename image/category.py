6523f337fbb52b56fc9e8e9c82961e78779e16d6
imhireki <hireki@proton.me>
2025-02-06 16:12:54 -0300

    [refactor]: rename support module and prefix to category

--- image/category.py
+++ image/category.py
@@ -10,7 +10,7 @@ ImageProfile = Type[IStaticImageProfile] | Type[IAnimatedImageProfile] | None
 SupportedImages = dict[str, dict[str, ImageProfile]]
 
 
-class IImageSupport(ABC):
+class IImageCategory(ABC):
     def __init__(self, image: PIL.Image.Image, supported_images: SupportedImages):
         self._image: PIL.Image.Image = image
         self._supported_images: SupportedImages = supported_images
@@ -19,40 +19,40 @@ class IImageSupport(ABC):
     def get_image_profile(self) -> ImageProfile: pass
 
 
-class StaticImageSupport(IImageSupport):
+class StaticImageCategory(IImageCategory):
     def get_image_profile(self) -> ImageProfile:
         format_mode = '_'.join([self._image.format or '', self._image.mode])
         return self._supported_images['STATIC'].get(format_mode)
 
 
-class AnimatedImageSupport(IImageSupport):
+class AnimatedImageCategory(IImageCategory):
     def get_image_profile(self) -> ImageProfile:
         format_mode = '_'.join([self._image.format or '', self._image.mode])
         return self._supported_images['ANIMATED'].get(format_mode)
 
 
-class ImageSupportProxy(IImageSupport):
-    _image_support: IImageSupport
+class ImageCategoryProxy(IImageCategory):
+    _image_category: IImageCategory
     _image_profile: ImageProfile
 
-    def get_image_support(self) -> IImageSupport:
-        if not hasattr(self, '_image_support'):
+    def get_image_category(self) -> IImageCategory:
+        if not hasattr(self, '_image_category'):
             # Determine between static or animated
             if (getattr(self._image, 'is_animated', False) is False
                 and self._image.format != 'GIF'):
-                self._image_support = StaticImageSupport(
+                self._image_category = StaticImageCategory(
                     self._image, self._supported_images)
             else:
-                self._image_support = AnimatedImageSupport(
+                self._image_category = AnimatedImageCategory(
                     self._image, self._supported_images)
 
-        return self._image_support
+        return self._image_category
 
     def get_image_profile(self) -> ImageProfile:
-        self.get_image_support()
+        self.get_image_category()
 
         if not hasattr(self, '_image_profile'):
-            self._image_profile = self._image_support.get_image_profile()
+            self._image_profile = self._image_category.get_image_profile()
         return self._image_profile
 
     def is_supported(self) -> bool:
