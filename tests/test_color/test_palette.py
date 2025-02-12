11e64446caf937af6c07b87788ca6547f2b00ab5
imhireki <hireki@proton.me>
2025-02-12 19:21:31 -0300

    [test]: add TestHexRGB

--- tests/test_color/test_palette.py
+++ tests/test_color/test_palette.py
@@ -26,3 +26,20 @@ def test_range_color_palette(mocker):
         palette_data_as_hex
         == [range_color_palette._hexlify_rgb(color) for color in sorted_colors][:2]
     )
+
+
+class TestHexRGB:
+    def test_get_color_bands(self, mocker, rgba_bands):
+        image = mocker.Mock(getdata=lambda band: rgba_bands[band])
+        color = palette.HexRGB(image)
+        assert color.get_color_bands() == rgba_bands[:3]
+
+    def test_structure_raw_palette(self, mocker, rgba_bands):
+        rgb_to_hex = mocker.patch("color.utils.rgb_to_hex", return_value="#ffffff")
+        color = palette.HexRGB(mocker.Mock())
+
+        rp = list(color.structure_raw_palette(rgba_bands[:3]))
+
+        rgb_to_hex.assert_any_call((1, 1, 1))
+        rgb_to_hex.assert_any_call((2, 2, 2))
+        assert rp == ["#ffffff"] * 2
