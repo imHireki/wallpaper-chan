bb5f518ce4577316ba69177ca687e188fc58022f
imhireki <hireki@proton.me>
2025-02-12 20:05:17 -0300

    [test]: add TestRGBA

--- tests/test_color/test_palette.py
+++ tests/test_color/test_palette.py
@@ -43,3 +43,15 @@ class TestHexRGB:
         rgb_to_hex.assert_any_call((1, 1, 1))
         rgb_to_hex.assert_any_call((2, 2, 2))
         assert rp == ["#ffffff"] * 2
+
+
+class TestRGBA:
+    def test_get_color_bands(self, mocker, rgba_bands):
+        image = mocker.Mock(getdata=lambda band: rgba_bands[band])
+        color = palette.RGBA(image)
+        assert color.get_color_bands() == rgba_bands
+
+    def test_structure_raw_palette(self, mocker, rgba_bands):
+        color = palette.RGBA(mocker.Mock())
+        rp = list(color.structure_raw_palette(rgba_bands))
+        assert rp == [(1, 1, 1, 1), (2, 2, 2, 2)]
