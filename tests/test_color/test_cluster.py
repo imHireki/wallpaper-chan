6cb7475271ce85e7acfa787e2eca42dab520ed8c
imhireki <hireki@proton.me>
2025-02-14 17:00:03 -0300

    [test]: add test_sorted_color_cluster

--- tests/test_color/test_cluster.py
+++ tests/test_color/test_cluster.py
@@ -14,3 +14,15 @@ def test_color_cluster(mocker):
     colors = color_cluster.get_colors()
 
     assert colors == expected_sorted_colors
+
+
+def test_sorted_color_cluster(mocker):
+    color = mocker.Mock(
+        get_color_bands=lambda: [],
+        structure_raw_palette=lambda _: ["blue", "blue", "blue", "red", "white", "red"],
+    )
+
+    pixel_cluster = cluster.SortedColorCluster(color)
+    palette = pixel_cluster.get_palette()
+
+    assert palette == ["blue", "red", "white"]
