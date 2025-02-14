3f8765bf6dcd3e2b1a3942b516388ec5faeda961
imhireki <hireki@proton.me>
2025-02-14 17:00:03 -0300

    [test]: add SortedColorCluster

--- color/cluster.py
+++ color/cluster.py
@@ -1,6 +1,11 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
 from typing import Iterator
 
-import PIL.Image
+from PIL.Image import Image
+
+if TYPE_CHECKING:
+    from color.palette import ColorBands, ColorIterator, Color, IColor
 
 
 SplitRGB = list[list[int]]
@@ -8,8 +13,8 @@ RGB = tuple[int, ...]
 
 
 class ColorCluster:
-    def __init__(self, image: PIL.Image.Image) -> None:
-        self._image: PIL.Image.Image = image
+    def __init__(self, image: Image) -> None:
+        self._image: Image = image
 
     def get_colors(self) -> list[RGB]:
         rgb_sequences = self._get_rgb_sequences()
@@ -38,3 +43,35 @@ class ColorCluster:
             key=lambda color_with_incidences: color_with_incidences[1],
             reverse=True,
         )
+
+
+class SortedColorCluster:
+    def __init__(self, color: IColor) -> None:
+        self.color = color
+
+    def get_palette(self) -> list[Color]:
+        color_bands = self.get_color_bands()
+        raw_palette = self.structure_raw_palette(color_bands)
+
+        color_counts = self._count_colors(raw_palette)
+        sorted_palette = self._sort_colors_by_count(color_counts)
+
+        return sorted_palette
+
+    def get_color_bands(self) -> ColorBands:
+        return self.color.get_color_bands()
+
+    def structure_raw_palette(self, color_bands: ColorBands) -> ColorIterator:
+        return self.color.structure_raw_palette(color_bands)
+
+    @staticmethod
+    def _count_colors(raw_palette: ColorIterator) -> dict[Color, int]:
+        color_counts: dict[Color, int] = {}
+
+        for color in raw_palette:
+            color_counts[color] = color_counts.get(color, 0) + 1
+        return color_counts
+
+    @staticmethod
+    def _sort_colors_by_count(color_counts: dict[Color, int]) -> list[Color]:
+        return sorted(color_counts, key=lambda key: color_counts[key], reverse=True)
