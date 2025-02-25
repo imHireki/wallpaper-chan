82e27d649c9a7bf93861586adaf64e34b1872cfc
imhireki <hireki@proton.me>
2025-02-24 23:36:48 -0300

    [refactor]: remove deprecated result, and instead, use a dynamic output

--- image/editor.py
+++ image/editor.py
@@ -1,10 +1,9 @@
-from tempfile import NamedTemporaryFile, _TemporaryFileWrapper
-from typing import Generator, Any, Literal, IO
+from typing import Any, Literal, IO, Iterator
 from abc import ABC, abstractmethod
 
 from PIL._typing import StrOrBytesPath
 from PIL.Image import Image, Resampling
-import PIL.ImageSequence
+from PIL import ImageSequence
 
 from image import utils
 
@@ -57,27 +56,17 @@ class StaticImageEditor(IImageEditor):
 
 
 class AnimatedImageEditor(IImageEditor):
-    _edited_frames: Generator[PIL.Image.Image, None, None]
-
-    def __init__(self, image: PIL.Image.Image) -> None:
-        self._original_image: PIL.Image.Image = image
-        self._result: _TemporaryFileWrapper = get_named_temporary_file()
+    def __init__(self, image: Image) -> None:
+        self._original_image: Image = image
+        self._processed_frames: Iterator = self._get_frames()
         self._actual_mode: str = self._find_actual_mode()
 
     @property
     def actual_mode(self) -> str:
         return self._actual_mode
 
-    @property
-    def result(self) -> _TemporaryFileWrapper:
-        return self._result
-
-    @result.deleter
-    def result(self) -> None:
-        self._result = get_named_temporary_file()
-
     def convert_mode(self, mode: str) -> None:
-        self._edited_frames = (
+        self._processed_frames = (
             frame.convert(mode=mode) if frame.mode != self.actual_mode else frame.copy()
             for frame in self._get_frames()
         )
@@ -90,7 +79,7 @@ class AnimatedImageEditor(IImageEditor):
             "resample": resample,
             "reducing_gap": reducing_gap,
         }
-        self._edited_frames = (
+        self._processed_frames = (
             (
                 frame.convert(self.actual_mode).resize(**resize_options)
                 if frame.mode != self.actual_mode
@@ -99,27 +88,28 @@ class AnimatedImageEditor(IImageEditor):
             for frame in self._get_frames()
         )
 
-    def save(self, format: str, **extra_options: Any) -> None:
-        if not self._edited_frames:
-            self.convert_mode(self.actual_mode)
-        first_frame = next(self._edited_frames)
+    def save(self, output: File, format: str, **extra_options: Any) -> None:
+        first_frame = next(self._processed_frames)
 
+        # Prepare to save all frames (to save as an animated image)
         if "save_all" in extra_options:
-            extra_options.update(append_images=list(self._edited_frames))
-        del self._edited_frames
+            extra_options.update(append_images=list(self._processed_frames))
 
-        with open(self._result.name, "wb") as temporary_file:
-            first_frame.save(temporary_file, format=format, **extra_options)
+        # Delete all the extra frames (to save as a static image)
+        del self._processed_frames
+
+        first_frame.save(output, format=format, **extra_options)
 
     def _find_actual_mode(self) -> str:
         if self._original_image.mode == "RGBA":
-            return "RGBA" if has_translucent_alpha(self._original_image) else "RGB"
+            return (
+                "RGBA" if utils.has_translucent_alpha(self._original_image) else "RGB"
+            )
         has_transparency = "transparency" in self._original_image.info
         return "RGB" if not has_transparency else "RGBA"
 
-    def _get_frames(self) -> PIL.ImageSequence.Iterator:
-        return PIL.ImageSequence.Iterator(self._original_image)
-
+    def _get_frames(self) -> ImageSequence.Iterator:
+        return ImageSequence.Iterator(self._original_image)
 
 def bulk_resize(editor: IImageEditor, resize_save_options: list[dict]):
     for options in resize_save_options:
