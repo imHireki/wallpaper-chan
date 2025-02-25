bba6abb07e16544083539ec4de53a1887f81d24f
imhireki <hireki@proton.me>
2025-02-25 12:39:39 -0300

    [feat]: add bulk_resize and bulk_resize_tempfile

--- image/utils.py
+++ image/utils.py
@@ -1,4 +1,14 @@
+from __future__ import annotations
+from tempfile import NamedTemporaryFile
+from io import BytesIO
+
 import PIL.Image
+
+from typing import TYPE_CHECKING, Generator
+
+
+if TYPE_CHECKING:
+    from image.editor import IImageEditor
 
 
 def has_translucent_alpha(image: PIL.Image.Image) -> bool:
@@ -7,3 +17,26 @@ def has_translucent_alpha(image: PIL.Image.Image) -> bool:
     if isinstance(bands_min_max[0], tuple) and len(bands_min_max) == 4:  # RGBA
         return bands_min_max[3][0] < 255
     return False
+
+
+def bulk_resize(
+    editor: IImageEditor, resize_save_options: list[dict]
+) -> Generator[BytesIO]:
+    """Resize images to BytesIO objects"""
+
+    for options in resize_save_options:
+        editor.resize(**options["resize"])
+        result = BytesIO()
+        editor.save(result, **options["save"])
+        yield result
+
+
+def bulk_resize_tempfile(
+    editor: IImageEditor, resize_save_options: list[dict]
+) -> Generator[str]:
+    for options in resize_save_options:
+        editor.resize(**options["resize"])
+        tempfile = NamedTemporaryFile(delete=False)
+        editor.save(tempfile, **options["save"])
+        tempfile.close()
+        yield tempfile.name
