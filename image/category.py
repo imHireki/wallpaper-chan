49a4816e5f2800e3499008752f99913438f25c2a
imhireki <hireki@proton.me>
2025-02-25 17:34:33 -0300

    [refactor]: rename the category classes to make them shorter

--- image/category.py
+++ image/category.py
@@ -13,51 +13,51 @@ Profile = IStaticProfile | IOptimizableStaticProfile | IOptimizableAnimatedProfi
 SupportedImages = dict[str, dict[str, type[Profile]]]
 
 
-class IImageCategory(ABC):
+class ICategory(ABC):
     def __init__(self, image: Image, supported_images: SupportedImages):
         self._image: Image = image
         self._supported_images: SupportedImages = supported_images
 
     @abstractmethod
-    def get_image_profile(self) -> Profile | None:
+    def get_profile(self) -> Profile | None:
         pass
 
 
-class StaticImageCategory(IImageCategory):
-    def get_image_profile(self) -> Profile | None:
+class StaticCategory(ICategory):
+    def get_profile(self) -> Profile | None:
         format_mode = "_".join([self._image.format or "", self._image.mode])
         profile_class = self._supported_images["STATIC"].get(format_mode)
         return profile_class(self._image) if profile_class else None
 
 
-class AnimatedImageCategory(IImageCategory):
-    def get_image_profile(self) -> Profile | None:
+class AnimatedCategory(ICategory):
+    def get_profile(self) -> Profile | None:
         format_mode = "_".join([self._image.format or "", self._image.mode])
         profile_class = self._supported_images["ANIMATED"].get(format_mode)
         return profile_class(self._image) if profile_class else None
 
 
-class ImageCategoryProxy(IImageCategory):
-    _image_category: IImageCategory
-    _image_profile: Profile | None
+class ImageCategoryProxy(ICategory):
+    _category: ICategory
+    _profile: Profile | None
 
-    def _determine_category(self) -> IImageCategory:
+    def _determine_category(self) -> ICategory:
         if (
             getattr(self._image, "is_animated", False) is False
             and self._image.format != "GIF"
         ):
-            return StaticImageCategory(self._image, self._supported_images)
+            return StaticCategory(self._image, self._supported_images)
         else:
-            return AnimatedImageCategory(self._image, self._supported_images)
+            return AnimatedCategory(self._image, self._supported_images)
 
-    def get_image_category(self) -> IImageCategory:
+    def get_category(self) -> ICategory:
         if not hasattr(self, "_image_category"):
             self._image_category = self._determine_category()
         return self._image_category
 
-    def get_image_profile(self) -> Profile | None:
-        self.get_image_category()
+    def get_profile(self) -> Profile | None:
+        self.get_category()
 
         if not hasattr(self, "_image_profile"):
-            self._image_profile = self._image_category.get_image_profile()
+            self._image_profile = self._image_category.get_profile()
         return self._image_profile
--- tests/test_image/test_category.py
+++ tests/test_image/test_category.py
@@ -3,25 +3,25 @@ import pytest
 from image import category
 
 
-def test_static_image_category(mocker):
+def test_static_category(mocker):
     profile_class = mocker.Mock()
     supported_images = {"STATIC": {"JPEG_RGB": profile_class}}
     image = mocker.Mock(format="JPEG", mode="RGB")
 
-    _category = category.StaticImageCategory(image, supported_images)
-    profile = _category.get_image_profile()
+    _category = category.StaticCategory(image, supported_images)
+    profile = _category.get_profile()
 
     assert profile is profile_class.return_value
     profile_class.assert_called_with(image)
 
 
-def test_animated_image_category(mocker):
+def test_animated_category(mocker):
     profile_class = mocker.Mock()
     supported_images = {"ANIMATED": {"GIF_P": profile_class}}
     image = mocker.Mock(format="GIF", mode="P")
 
-    _category = category.AnimatedImageCategory(image, supported_images)
-    profile = _category.get_image_profile()
+    _category = category.AnimatedCategory(image, supported_images)
+    profile = _category.get_profile()
 
     assert profile is profile_class.return_value
     profile_class.assert_called_with(image)
@@ -31,9 +31,9 @@ class TestImageCategoryProxy:
     @pytest.mark.parametrize(
         "is_animated, format, category_patch",
         [
-            [True, "GIF", "image.category.AnimatedImageCategory"],
-            [False, "JPEG", "image.category.StaticImageCategory"],
-            [False, "GIF", "image.category.AnimatedImageCategory"],
+            [True, "GIF", "image.category.AnimatedCategory"],
+            [False, "JPEG", "image.category.StaticCategory"],
+            [False, "GIF", "image.category.AnimatedCategory"],
         ],
     )
     def test_determine_category(self, mocker, is_animated, format, category_patch):
@@ -45,21 +45,21 @@ class TestImageCategoryProxy:
 
         assert _category is category_patch.return_value
 
-    def test_get_image_category(self, mocker):
+    def test_get_category(self, mocker):
         proxy = category.ImageCategoryProxy(mocker.Mock(), {})
-        _category = proxy.get_image_category()
-        _category_2 = proxy.get_image_category()
+        _category = proxy.get_category()
+        _category_2 = proxy.get_category()
 
         assert _category is proxy._image_category
         assert _category_2 is _category
 
-    def test_get_image_profile(self, mocker):
+    def test_get_profile(self, mocker):
         proxy = category.ImageCategoryProxy(mocker.Mock(), {})
-        mocker.patch.object(proxy, "get_image_category")
+        mocker.patch.object(proxy, "get_category")
         proxy._image_category = mocker.Mock()
 
-        _profile = proxy.get_image_profile()
-        _profile_2 = proxy.get_image_profile()
+        _profile = proxy.get_profile()
+        _profile_2 = proxy.get_profile()
 
         assert _profile is proxy._image_profile
         assert _profile_2 is _profile
